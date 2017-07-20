4ba3a2363f9ee197693a16742a35f0a7a6bab6fa
Ricardo M. Lima <ricardolima@neemu.com>
2017-07-20 02:47:30 -0300

    creating user form using plain password field

--- shipping-service/src/AppBundle/Form/UserType.php
+++ shipping-service/src/AppBundle/Form/UserType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +15,14 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username')->add('password')->add('isActive');
+        $builder
+            ->add('username')
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('isActive');
     }
     
     /**
